6a11454ba90dc01208098482f179472c5e242be8
Arunava-27 <drarunkundu22@gmail.com>
2022-03-28 17:16:34 +0530

    php variables

---
+++ variables.php
@@ -0,0 +1,24 @@
+<?php
+echo "tutorial for variables <br>";
+// declare variables with $ 
+$name = "Arunava";
+$income = 5000000;
+$weight = 70.1;
+echo "\n this guy is $name and income is Rs.$income <br>";
+echo "\n His weight is $weight"; 
+echo "<h3>Rules for creating php variables</h3>";
+?>
+<html>
+    <head>
+        <title>PHP_VARIABLES</title>
+    </head>
+    <body>
+        <ul>
+            <li>Starts with a $ sign</li>
+            <li>Cannot Start with a number</li>
+            <li>Must start with a letter or '_'</li>            
+            <li>Can only contain alphanumeric characters and underscore</li>           
+            <li>Variables declared are case sensitive</li>
+        </ul>
+    </body>
+</html>
